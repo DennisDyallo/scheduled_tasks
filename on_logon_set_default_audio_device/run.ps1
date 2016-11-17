4a8c12f25033fb1861416a9e28bd8c61cd45acb8
Dennis Dyall <dyallo@live.se>
2016-11-18 00:16:36 +0100

    new symlinks

--- on_logon_set_default_audio_device/run.ps1
+++ on_logon_set_default_audio_device/run.ps1
@@ -1,1 +1,10 @@
-Set-DefaultAudioDevice "Line Out (Scarlett 2i4 USB)"
+param($device)
+Write-Host $device;
+if($device -eq 0){
+    Set-DefaultAudioDevice "Line Out (Scarlett 2i4 USB)";
+}
+elseif($device -eq 1){
+	Set-DefaultAudioDevice "SAMSUNG-4 (NVIDIA High Definition Audio)";
+	
+}
+	
