fdfeb001f467dd9ae3a66a51bbb44d3db2bba182
Dennis Dyall <dyallo@live.se>
2017-05-20 22:30:32 +0200

    updated device id string.. not working but it should

--- on_logon_restart_scarlet_drivers/restart_scarlet_drivers.bat
+++ on_logon_restart_scarlet_drivers/restart_scarlet_drivers.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 for /f "tokens=*" %%a in ('C:\Windows\System32\WindowsPowerShell\v1.0\powershell get-date -format "{yyyy-MM-dd}"') do set Today=%%a
-devcon disable "@USB\VID_1235&PID_800A\5&6B4DCFF&0&3" >> %Today%.log
-devcon enable "@USB\VID_1235&PID_800A\5&6B4DCFF&0&3" >> %Today%.log
+SET focusriteScarletIdString="@USB\VID_1235&PID_800A\5&6B4DCFF&0&1"
+devcon disable %focusriteScarletIdString% >> %Today%.log
+devcon enable %focusriteScarletIdString% >> %Today%.log
