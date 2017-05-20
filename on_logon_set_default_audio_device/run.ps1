dae46792f8f4862627b4d327cc451c91df2d88a0
Dennis Dyall <dyallo@live.se>
2017-05-20 22:33:17 +0200

    updated audio device name

--- on_logon_set_default_audio_device/run.ps1
+++ on_logon_set_default_audio_device/run.ps1
@@ -1,7 +1,10 @@
+#required AudioDeviceCmdlets.dll from https://www.automatedops.com/projects/windowsaudiodevice-powershell-cmdlet/
+#Link: https://github.com/cdhunt/WindowsAudioDevice-Powershell-Cmdlet/blob/master/AudioDeviceCmdlets.zip
+#Get-AudioDeviceList to get the name
 param($device)
 Write-Host $device;
 if($device -eq 0){
-    Set-DefaultAudioDevice "Line Out (Scarlett 2i4 USB)";
+    Set-DefaultAudioDevice "Line Out (2- Scarlett 2i4 USB)";
 }
 elseif($device -eq 1){
 	Set-DefaultAudioDevice "SAMSUNG-4 (NVIDIA High Definition Audio)";
