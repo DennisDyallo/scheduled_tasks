d5f4ed6903a2cf58bea501c225aea1dc94239e21
Dennis Dyall <dyallo@live.se>
2017-05-20 22:31:09 +0200

    added comment

--- on_logon_restart_scarlet_drivers/restart_scarlet_drivers.bat
+++ on_logon_restart_scarlet_drivers/restart_scarlet_drivers.bat
@@ -1,4 +1,8 @@
 @ECHO OFF
+
+rem devcon find *
+rem get the USB audio scarlet
+rem set as focusriteScarletIdString
 for /f "tokens=*" %%a in ('C:\Windows\System32\WindowsPowerShell\v1.0\powershell get-date -format "{yyyy-MM-dd}"') do set Today=%%a
 SET focusriteScarletIdString="@USB\VID_1235&PID_800A\5&6B4DCFF&0&1"
 devcon disable %focusriteScarletIdString% >> %Today%.log
